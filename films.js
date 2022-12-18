032800897c2bfad7ef0e4cb0105b5fae284b4f4d
Oleg <email@gmail.com>
2022-12-19 00:31:14 +0300

    new filmsDB

---
+++ films.js
@@ -0,0 +1,20 @@
+const number0Films = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    'count': number0Films,
+    'movies':{},
+    'actors':{},
+    'genres':[],
+    'privat': false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
